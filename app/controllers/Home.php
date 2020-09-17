a9c55d8fcf4ff5c8724dee1be2bbcfa7b0b5972f
feriferdinan <feri.ferdinan79@gmail.com>
2020-09-17 20:14:06 +0700

    cors

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,4 +1,9 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 use Ayesh\InstagramDownload\InstagramDownload;
 
