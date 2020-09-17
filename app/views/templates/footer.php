af1b896bfc4ae1858f0b771e99adcd51c1a28488
feriferdinan <feri.ferdinan79@gmail.com>
2020-09-17 14:52:46 +0700

    update

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -47,11 +47,11 @@
 
 
 <!-- Bootstrap core JavaScript-->
-<script src="<?= BASEURL ?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= BASEURL ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= BASEURL ?>js/vendor/jquery/jquery.min.js"></script>
+<script src="<?= BASEURL ?>js/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?= BASEURL ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?= BASEURL ?>js/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
 <script src="<?= BASEURL ?>js/sb-admin-2.min.js"></script>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -12,7 +12,7 @@
   <title><?= $data['title']; ?></title>
 
   <!-- Custom fonts for this template-->
-  <link href="<?= BASEURL; ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= BASEURL; ?>js/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
