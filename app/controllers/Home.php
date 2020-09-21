fecae437ac370b30b780b5f2670675ba722b0d7c
feriferdinan <feri.ferdinan79@gmail.com>
2020-09-21 14:14:10 +0700

    update

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,9 +1,13 @@
 <?php
 
-
-
 class Home extends Controller
 {
+    const TWITTER_VIDEO_PLAYER_INFO = 'https://twitter.com/i/videos/tweet/';
+    const TWITTER_VIDEO_INFO_API = 'https://api.twitter.com/1.1/statuses/show/';
+    const INSTAGRAM_DOMAIN = 'instagram.com';
+    const TWITTER_DOMAIN = 'twitter.com';
+    const FACEBOOK_DOMAIN = 'facebook.com';
+
     public function index()
     {
         $data['title'] = 'Save-Me';
@@ -13,21 +17,17 @@ class Home extends Controller
         $this->view('templates/footer', $data);
     }
 
-    public function instagram($url = "https://www.instagram.com/p/CFRVRkNFT0w/?utm_source=ig_web_copy_link")
+    private function instagram($url = "https://www.facebook.com/watch/?v=3177349105706517&extid=YdaVVNoj809o5Ae4")
     {
-        require_once './app/core/InstagramDownload.php';
         try {
-            $_SERVER['HTTP_USER_AGENT'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0';
-            $client = new InstagramDownload("https://www.instagram.com/p/CFRVRkNFT0w/?utm_source=ig_web_copy_link");
-            $url = $client->getDownloadUrl(); // Returns the download URL.
-            $type = $client->getType(); // Returns "image" or "video" depending on the media type.
-            $url_parsed = \parse_url($url);
+            $HTML = $this->fetch($url);
+            $parsed = $this->parse_meta($HTML);
+            $res = $this->process_meta($parsed);
             return [
                 "status" => true,
-                "data" => $url,
-                "host" => $url_parsed["host"],
-                "type" => $type,
-                "message" => $type,
+                "data" => $res["download_url"],
+                "type" => $res["type"],
+                "message" => $res["type"],
             ];
         } catch (\InvalidArgumentException $exception) {
             return [
@@ -44,31 +44,110 @@ class Home extends Controller
         }
     }
 
-    public function instastory($username = "berlliana.lovell")
+    private function facebook($url)
     {
-        require_once './app/core/InstaStory.php';
-        $i = new InstaStory();
-        $i->getStory($username);
-    }
-
-    public function twitter($url = "https://twitter.com/i/status/1306767468971646977")
-    {
-        require_once './app/core/TwitterDownload.php';
         try {
-            $t = new TwitterDownload();
-            $result =  $t->download($url);
-            if (isset($result["variants"])) {
-                $url_parsed = \parse_url($url);
+            $HTML = $this->fetch($url);
+            $parsed = $this->parse_meta($HTML);
+            $data = $this->process_meta($parsed);
+            if ($data["type"] == "video") {
+                $html_encoded = htmlentities($HTML);
+                $videoHD = "";
+                $videoSD = "";
+                $re = '/hd_src:"(.*?)",sd_src/m';
+                preg_match_all($re, $HTML, $matches, PREG_SET_ORDER, 0);
+                if ($matches && is_array($matches) && sizeof($matches) > 0) {
+                    $matches = $matches[0];
+                    if ($matches && is_array($matches) && sizeof($matches) > 1) $videoHD = $matches[1];
+                }
+
+                $re = '/sd_src_no_ratelimit:"(.*?)"/m';
+                preg_match_all($re, $HTML, $matches, PREG_SET_ORDER, 0);
+                if ($matches && is_array($matches) && sizeof($matches) > 0) {
+                    $matches = $matches[0];
+                    if ($matches && is_array($matches) && sizeof($matches) > 1) $videoSD = $matches[1];
+                }
+                if ($videoHD || $videoSD) {
+                    return [
+                        "status" => true,
+                        "type" => "video",
+                        "message" => "Success",
+                        "data" => [
+                            "videoHD" => $videoHD,
+                            "videoSD" => $videoSD,
+                        ],
+                    ];
+                }
+            } else {
                 return [
                     "status" => true,
-                    "type" => "video",
-                    "host" => $url_parsed["host"],
-                    "data" => [
-                        "videoSD" => $result["variants"][2]->url,
-                        "videoHD" => $result["variants"][3]->url,
-                    ],
-                    "message" => "Success",
+                    "data" => $data["download_url"],
+                    "type" => $data["type"],
+                    "message" => $data["type"],
                 ];
+            }
+        } catch (\InvalidArgumentException $exception) {
+            return [
+                "status" => false,
+                "data" => "",
+                "message" => $exception->getMessage(),
+            ];
+        } catch (\RuntimeException $exception) {
+            return [
+                "status" => false,
+                "data" => "",
+                "message" => $exception->getMessage(),
+            ];
+        }
+    }
+
+
+
+    private function twitter($url = "https://twitter.com/NetflixID/status/1307922507291176960")
+    {
+        try {
+            $urlDetails = $this->extractIdFromUrl($url);
+
+            $bearerToken = $this->getAuthorizationToken($urlDetails, self::TWITTER_VIDEO_PLAYER_INFO);
+
+            $response = $this->fetch(self::TWITTER_VIDEO_INFO_API . $urlDetails['tweetId'] . '.json?include_entities=true', [
+                "Authorization: {$bearerToken}"
+            ]);
+
+            $jsonResponse = json_decode($response);
+            $result = [];
+            $text = $jsonResponse->text;
+            if (isset($jsonResponse->extended_entities)) {
+                $thumbnail = $jsonResponse->extended_entities->media[0]->media_url_https;
+                $url = $jsonResponse->extended_entities->media[0]->url;
+                $type = $jsonResponse->extended_entities->media[0]->type;
+
+                $variants = [];
+                if (isset($jsonResponse->extended_entities->media[0]->video_info->variants))
+                    $variants = $jsonResponse->extended_entities->media[0]->video_info->variants;
+
+                $result = compact("text", "thumbnail", "url", "type", "variants");
+            }
+
+            if (isset($result)) {
+                if ($result["type"] == "video") {
+                    return [
+                        "status" => true,
+                        "type" => "video",
+                        "data" => [
+                            "videoSD" => $result["variants"][2]->url,
+                            "videoHD" => $result["variants"][3]->url,
+                        ],
+                        "message" => "Success",
+                    ];
+                } else {
+                    return [
+                        "status" => true,
+                        "type" => "image",
+                        "data" => $result["thumbnail"],
+                        "message" => "Success",
+                    ];
+                }
             } else {
                 return [
                     "status" => false,
@@ -86,54 +165,6 @@ class Home extends Controller
         }
     }
 
-    public function facebook_video($url)
-    {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U;   Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        $html_encoded = htmlentities($data);
-        $videoHD = "";
-        $videoSD = "";
-        $re = '/hd_src:"(.*?)",sd_src/m';
-        preg_match_all($re, $data, $matches, PREG_SET_ORDER, 0);
-        if ($matches && is_array($matches) && sizeof($matches) > 0) {
-            $matches = $matches[0];
-            if ($matches && is_array($matches) && sizeof($matches) > 1) $videoHD = $matches[1];
-        }
-
-        $re = '/sd_src_no_ratelimit:"(.*?)"/m';
-        preg_match_all($re, $data, $matches, PREG_SET_ORDER, 0);
-        if ($matches && is_array($matches) && sizeof($matches) > 0) {
-            $matches = $matches[0];
-            if ($matches && is_array($matches) && sizeof($matches) > 1) $videoSD = $matches[1];
-        }
-        $url_parsed = \parse_url($url);
-        if ($videoHD || $videoSD) {
-            return [
-                "status" => true,
-                "type" => "video",
-                "host" => $url_parsed["host"],
-                "message" => "Success",
-                "data" => [
-                    "videoHD" => $videoHD,
-                    "videoSD" => $videoSD,
-                ],
-            ];
-        } else {
-            return [
-                "status" => false,
-                "host" => $url_parsed["host"],
-                "message" => "Cannot Find Video",
-                "data" => "",
-            ];
-        }
-    }
-
 
     public function download()
     {
@@ -141,22 +172,27 @@ class Home extends Controller
         $result = [];
         $url_parsed = \parse_url($url);
         switch ($url_parsed['host']) {
-            case 'instagram.com':
-            case 'www.instagram.com':
+            case self::INSTAGRAM_DOMAIN:
+            case 'www.' . self::INSTAGRAM_DOMAIN:
                 $result = $this->instagram($url);
                 break;
-            case 'twitter.com':
-            case 'www.twitter.com':
+            case self::TWITTER_DOMAIN:
+            case 'www.' . self::TWITTER_DOMAIN:
                 $result = $this->twitter($url);
                 break;
-            case 'facebook.com':
-            case 'www.facebook.com':
-                $result = $this->facebook_video($url);
+            case self::FACEBOOK_DOMAIN:
+            case 'www.' . self::FACEBOOK_DOMAIN:
+                $result = $this->facebook($url);
                 break;
             default:
+                $result =  [
+                    "status" => false,
+                    "data" => "",
+                    "message" => "Invalid URL ",
+                ];
                 break;
         }
-
+        $result['host'] =  $url_parsed['host'];
         echo json_encode($result);
     }
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -38,4 +38,148 @@ class Controller
         curl_close($ch);
         return $status;
     }
+    public function fetch(string $url, array $headers = []): string
+    {
+        $curl = \curl_init($url);
+
+        if (!$curl) {
+            throw new \RuntimeException('Unable to initialize curl.', 12);
+        }
+
+        \curl_setopt($curl, \CURLOPT_FAILONERROR, true);
+        \curl_setopt($curl, \CURLOPT_FOLLOWLOCATION, true);
+        \curl_setopt($curl, \CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($curl, \CURLOPT_TIMEOUT, 1500);
+
+        if (!empty($headers)) {
+            \curl_setopt($curl, \CURLOPT_HTTPHEADER, $headers);
+        }
+        \curl_setopt($curl, \CURLOPT_USERAGENT,  "Mozilla/5.0 (Windows; U;   Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
+
+        // if (!empty($_SERVER['HTTP_USER_AGENT'])) {
+        //     \curl_setopt($curl, \CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        // }
+
+        \curl_setopt($curl, \CURLOPT_SSL_VERIFYPEER, false);
+
+        \curl_setopt($curl, \CURLOPT_HEADER, false);
+        \curl_setopt($curl, \CURLOPT_CONNECTTIMEOUT, 120);
+
+        $response = \curl_exec($curl);
+
+        \curl_close($curl);
+
+        if (!empty($response) && \is_string($response)) {
+            return $response;
+        }
+
+        throw new \RuntimeException('Could not fetch data.');
+    }
+
+    public function parse_meta($HTML)
+    {
+        $raw_tags = [];
+        $meta_values = [];
+
+        \preg_match_all('/<meta[^>]+="([^"]*)"[^>]' . '+content="([^"]*)"[^>]+>/i', $HTML, $raw_tags);
+        if (!empty($raw_tags)) {
+            $multi_value_tags = \array_unique(\array_diff_assoc($raw_tags[1], \array_unique($raw_tags[1])));
+            foreach ($raw_tags[1] as $i => $tag) {
+                $has_multiple_values = false;
+
+
+                foreach ($multi_value_tags as $multi_tag) {
+                    if ($tag === $multi_tag) {
+                        $has_multiple_values = true;
+                    }
+                }
+
+                if ($has_multiple_values) {
+                    $meta_values[$tag][] = $raw_tags[2][$i];
+                } else {
+                    $meta_values[$tag] = $raw_tags[2][$i];
+                }
+            }
+        }
+
+        if (empty($meta_values)) {
+            return false;
+        }
+
+        return $meta_values;
+    }
+
+    public function validateUrl($url, $domain)
+    {
+
+        $url = \parse_url($url);
+        if (empty($url['host'])) {
+            throw new \InvalidArgumentException('Invalid URL');
+        }
+
+        $url['host'] = \strtolower($url['host']);
+
+        if ($url['host'] !== $domain && $url['host'] !== 'www.' . $domain) {
+            throw new \InvalidArgumentException('Entered URL is not an ' . $domain . ' URL.');
+        }
+
+        if (empty($url['path'])) {
+            throw new \InvalidArgumentException('No image or video found in this URL');
+        }
+
+        $args = \explode('/', $url['path']);
+        if (!empty($args[1]) && ($args[1] === 'p' || $args[1] === 'tv') && isset($args[2]{
+            4}) && !isset($args[2]{
+            255})) {
+            return $args[2];
+        }
+
+        throw new \InvalidArgumentException('No image or video found in this URL');
+    }
+    public function extractIdFromUrl($url)
+    {
+        $tweetUrl = explode('?', $url)[0];
+        $tweetUser = explode('/', $tweetUrl)[3];
+        $tweetId = explode('/', $tweetUrl)[5];
+        return compact("tweetUrl", "tweetUser", "tweetId");
+    }
+
+    public function process_meta($meta)
+    {
+        if (!\is_array($meta)) {
+            throw new \RuntimeException('Error fetching information. Perhaps the post is private.', 3);
+        }
+        if (!empty($meta['og:video'])) {
+            return [
+                "type" => 'video',
+                'download_url' => $meta['og:video']
+            ];
+        } elseif (!empty($meta['og:image'])) {
+            return [
+                "type" => 'image',
+                'download_url' => $meta['og:image']
+            ];
+        } else {
+            throw new \RuntimeException('Error fetching information. Perhaps the post is private.', 4);
+        }
+    }
+
+    public function getAuthorizationToken($urlDetails, $api)
+    {
+        $url = $api . $urlDetails['tweetId'];
+
+        $response = $this->fetch($url);
+
+        if (\preg_match('/src="(.*)"/', $response, $matches)) {
+            $fileJsUrl = $matches[1];
+
+            $html = $this->fetch($fileJsUrl);
+
+            if (\preg_match('/Bearer ([a-zA-Z0-9%-])+/', $html, $matches)) {
+                $bearerToken = $matches[0];
+
+                return $bearerToken;
+            }
+        }
+    }
 }
